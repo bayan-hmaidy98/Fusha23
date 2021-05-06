12b8ad932a6f4ea0b3601b09982fc468758f0c4c
BayanHmaidy <>
2021-05-06 12:31:34 +0000

    Loops

--- app.js
+++ app.js
@@ -7,11 +7,28 @@ if (userAnswer === 'yes')
 
 else {
   document.write ('Hope you changed your mind.')
-
 }
+var userIdentity = prompt('Are you a parent, a child or neither?')
+  
+  while (!(userIdentity == 'child' || userIdentity == 'parent' || userIdentity == 'neither' ))
+  {
+    userIdentity = prompt('Try again please!')
+  }
 var btn = document.getElementById ("button")
 btn.addEventListener ("click",myFunction)
   function myFunction() {
-  confirm("Keep Going!") }
+  confirm("Keep Going!")
+  }
+var image = ''
+var numberOfpictures = prompt('How many Suggestions you would like to see?' )
 
-  console.log (userAnswer)
+
+for (var x = 1; x <= numberOfpictures; x++)
+{
+  image = image + '<img src= "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1539184176i/42271493._UY606_SS606_.jpg">'
+ 
+  }
+ document.write (image)
+ document.write ('You have asked for ' +numberOfpictures+ ' suggestions.') 
+  console.log(userAnswer)
+  
